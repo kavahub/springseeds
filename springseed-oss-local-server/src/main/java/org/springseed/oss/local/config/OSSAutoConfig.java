1239cb5c5a253cf06b1e2cea4e14ac76bf1244de
PinWei.Wan <44010039@qq.com>
2022-04-25 17:17:30 +0800

    oss-local

--- springseed-oss-local-server/src/main/java/org/springseed/oss/local/config/OSSAutoConfig.java
+++ springseed-oss-local-server/src/main/java/org/springseed/oss/local/config/OSSAutoConfig.java
@@ -11,6 +11,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties({OSSProperties.class})
-public class OSSConfig {
+public class OSSAutoConfig {
     
 }
