d1a31892e5181423e438bcb863e2b4fc5626acc0
PinWei.Wan <44010039@qq.com>
2022-04-29 15:57:32 +0800

    oss-local

--- springseed-oss-local-server/src/main/java/org/springseed/oss/local/config/RestResponseEntityExceptionHandler.java
+++ springseed-oss-local-server/src/main/java/org/springseed/oss/local/config/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,8 @@
 package org.springseed.oss.local.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springseed.core.util.ErrorResponse;
@@ -20,22 +22,25 @@ import lombok.extern.slf4j.Slf4j;
  * @since 1.0.0
  */
 @Slf4j
-//@ControllerAdvice
+@ControllerAdvice
 public class RestResponseEntityExceptionHandler {
     @ExceptionHandler({ MetadataNotFoundException.class, FileNotFoundException.class })
-    public ResponseEntity<ErrorResponse> handleMetadataNotFoundException(RuntimeException ex) {
+    public ResponseEntity<ErrorResponse> handleNotFound(RuntimeException ex) {
         if (log.isDebugEnabled()) {
             log.debug("{} - {}", ex.getClass().getName(), ex.getMessage());
         }
-        return ResponseEntity.notFound().build();
+
+        final ErrorResponse bodyOfResponse = new ErrorResponse(ex.getMessage(), ex.getClass().getName());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(bodyOfResponse);
     }
 
     @ExceptionHandler({ OSSLocalRuntimeException.class })
-    public ResponseEntity<ErrorResponse> handleOSSRuntimeException(RuntimeException ex) {
+    public ResponseEntity<ErrorResponse> handleBadRequest(RuntimeException ex) {
         if (log.isDebugEnabled()) {
             log.debug("{} - {}", ex.getClass().getName(), ex.getMessage());
         }
-        final ErrorResponse bodyOfResponse = new ErrorResponse(ex.getMessage(), ex.getClass().getName(), ExceptionUtil.stacktraceToString(ex));
+        final ErrorResponse bodyOfResponse = new ErrorResponse(ex.getMessage(), ex.getClass().getName());
         
         return ResponseEntity.badRequest().body(bodyOfResponse);
     }
